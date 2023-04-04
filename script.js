3d6550d986394c8c69a76276eadd8d455d708746
sofyzsy.st <119410757+sofijastarshaja@users.noreply.github.com>
2023-04-04 13:29:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const minusButton = document.querySelector('.js-minus');
+const plusButton = document.querySelector('.js-plus');
+const total = document.querySelector('.js-total');
+const thisMoveMinus - document.querySelector(', js-minus-this-move');
+const thisMoveMinus - document.querySelector(', js-minus-this-move');
+const thisMoveMinus - document.querySelector(', js-minus-this-move');
+const refreshButton - document.querySelector(',js-total')
+plusButton.addEventListener('click', () => {
+    total.innerHTML = Number(total.innerHTML) + 1
+    minusButton.addEventListener('click', () => {
+total.innerHTML = Number(total.innerHTML) - 1
+    })
+    thisMoveMinus.addEventListener('click', () => {
+        thisMoveTotal.innerHTML = Number(thisMoveTotal.innerHTML) - 1
+    });
+    
+    thisMovePlus.addEventListener('click', () => {
+        thisMoveTotal.innerHTML = Number(thisMoveTotal.innerHTML) + 1
+    })
